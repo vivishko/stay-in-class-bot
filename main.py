0a7cc88840ba7a350054577929a8b89b4ae291e1
villu <viola.smirnova2015@yandex.ru>
2021-02-27 19:33:10 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import discord
+import os
+import requests
+import json
+import random
+
+TOKEN = 'ODE1MTgzNTA5MTk0ODAxMTYz.YDoszQ.SV7zfzQtt_oST5Z9FNaA7-vACiY'
+client = discord.Client()
+
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
+starter_encouragements = ["Cheer up!", "Hang in there.", "You are a great person / bot!"]
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    msg = message.content
+    if msg.startswith('$hello'):
+        await message.channel.send('Hello!')
+    if msg.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
+    if any(word in msg for word in sad_words):
+        await message.channel.send(random.choice(starter_encouragements))
+
+client.run(TOKEN)
